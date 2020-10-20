6578b8d72e184b4010807ff2d239c3abee1a26e5
jlbuild <juliabuildbot@gmail.com>
2020-10-20 18:46:06 +0000

    Mesa_jll build 20.1.5+2

--- src/wrappers/i686-w64-mingw32.jl
+++ src/wrappers/i686-w64-mingw32.jl
@@ -2,52 +2,15 @@
 export opengl32sw
 
 using Zlib_jll
-## Global variables
-PATH = ""
-LIBPATH = ""
-LIBPATH_env = "PATH"
-LIBPATH_default = ""
-
-# Relative path to `opengl32sw`
-const opengl32sw_splitpath = ["bin", "opengl32sw.dll"]
-
-# This will be filled out by __init__() for all products, as it must be done at runtime
-opengl32sw_path = ""
-
-# opengl32sw-specific global declaration
-# This will be filled out by __init__()
-opengl32sw_handle = C_NULL
-
-# This must be `const` so that we can use it with `ccall()`
-const opengl32sw = "opengl32sw.dll"
-
-
-"""
-Open all libraries
-"""
+JLLWrappers.@generate_wrapper_header("Mesa")
+JLLWrappers.@declare_library_product(opengl32sw, "opengl32sw.dll")
 function __init__()
-    global artifact_dir = abspath(artifact"Mesa")
+    JLLWrappers.@generate_init_header(Zlib_jll)
+    JLLWrappers.@init_library_product(
+        opengl32sw,
+        "bin\\opengl32sw.dll",
+        RTLD_LAZY | RTLD_DEEPBIND,
+    )
 
-    # Initialize PATH and LIBPATH environment variable listings
-    global PATH_list, LIBPATH_list
-    # From the list of our dependencies, generate a tuple of all the PATH and LIBPATH lists,
-    # then append them to our own.
-    foreach(p -> append!(PATH_list, p), (Zlib_jll.PATH_list,))
-    foreach(p -> append!(LIBPATH_list, p), (Zlib_jll.LIBPATH_list,))
-
-    global opengl32sw_path = normpath(joinpath(artifact_dir, opengl32sw_splitpath...))
-
-    # Manually `dlopen()` this right now so that future invocations
-    # of `ccall` with its `SONAME` will find this path immediately.
-    global opengl32sw_handle = dlopen(opengl32sw_path)
-    push!(LIBPATH_list, dirname(opengl32sw_path))
-
-    # Filter out duplicate and empty entries in our PATH and LIBPATH entries
-    filter!(!isempty, unique!(PATH_list))
-    filter!(!isempty, unique!(LIBPATH_list))
-    global PATH = join(PATH_list, ';')
-    global LIBPATH = join(vcat(LIBPATH_list, [Sys.BINDIR, joinpath(Sys.BINDIR, Base.LIBDIR, "julia"), joinpath(Sys.BINDIR, Base.LIBDIR)]), ';')
-
-    
+    JLLWrappers.@generate_init_footer()
 end  # __init__()
-
